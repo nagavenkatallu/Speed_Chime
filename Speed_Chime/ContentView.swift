77235ea18ef64e32d96b31402837cbd1361b4a8c
Venkat Allu <venkatallu97@gmail.com>
2026-05-21 12:33:42 -0500

    Initial Commit

---
+++ Speed_Chime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Speed_Chime
+//
+//  Created by Tharun Thota on 5/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Speed_Chime/Speed_ChimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speed_ChimeApp.swift
+//  Speed_Chime
+//
+//  Created by Tharun Thota on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Speed_ChimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
